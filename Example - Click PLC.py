6137c5930d594c34a0d41be9386f4bd254651417
Nicholas Robinson <60759127+Freeno83@users.noreply.github.com>
2021-03-07 00:17:02 -0600

    Update Example - Click PLC.py

--- Example - Click PLC.py
+++ Example - Click PLC.py
@@ -3,14 +3,14 @@ from CPModbus import Instrument
 # https://www.automationdirect.com/adc/overview/catalog/programmable_controllers/click_series_plcs/click_plcs_(stackable_micro_brick)
 plc = Instrument(1, baudrate=115200)
 
-plc.write_bits(0x2000, [1]*6)
-print(plc.read_bits(0x2000, 6))
+plc.write_bits(0x2000, [1]*6) # Set all outputs high
+print(plc.read_bits(0x2000, 6)) # read the state of all outputs
 
-plc.write_registers(0x0000, [777, 888])
-print(plc.read_registers(0x0000, 2))
+plc.write_registers(0x0000, [777, 888]) # write values to 2 registers
+print(plc.read_registers(0x0000, 2)) # read values from 2 registers
 
-plc.write_long(0x4000, 555555555)
-print(plc.read_long(0x4000))
+plc.write_long(0x4000, 555555555) # Write a long value
+print(plc.read_long(0x4000)) # read from a double register
 
-plc.write_float(0x7000, 11.22)
-print(plc.read_float(0x7000))
+plc.write_float(0x7000, 11.22) # write a cloat value
+print(plc.read_float(0x7000)) # read a float value
